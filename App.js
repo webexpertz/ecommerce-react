48357e962e58375e853884877ad702e4a6925180
webexpertz <webexpertz.ntf@gmail.com>
2020-01-21 15:32:11 +0530

    setup

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/store/reducers';
+import Router from './src/native/views/Router';
+
+class App extends Component {  
+
+  render() {
+    
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
